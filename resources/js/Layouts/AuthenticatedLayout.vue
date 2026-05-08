ba7461d8acd6093cf7dd809875a296869fd655b6
Davi Jatobá Galdino <davijatobagaldino@gmail.com>
2026-05-07 21:07:09 -0300

    feat: add Chart.js dependency and configure localization settings in app.js

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -27,15 +27,15 @@ const items = [
 </script>
 
 <template>
-    <div class="min-h-screen bg">
+    <div class="min-h-screen bg-[#faf8f5]">
         <div class="max-w-7xl mx-auto flex flex-col sm:px-6 lg:px-8 gap-6 py-6 bg-red-500">
             <header>
                 <Menubar :model="items">
-                    <template #start></template>
+                    <template #start> Start </template>
                     <template #item="{ item }">
                         <Link :href="route(item.to)">{{ item.label }}</Link>
                     </template>
-                    <template #end></template>
+                    <template #end> End </template>
                 </Menubar>
             </header>
             <main class="flex-1 bg-green-500">
